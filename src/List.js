29fec22072ec275c7ff64ce97a5b1a1aa5e472c8
Mastan Patel <m.mastanpatel@gmail.com>
2022-01-21 21:15:34 +0530

    adde remove icon

--- src/List.js
+++ src/List.js
@@ -1,16 +1,24 @@
 import React from 'react';
-
+import {FaWindowClose} from 'react-icons/fa';
 const List = ({person}) => {
 
   const  {id, name, age, image} = person;
 
   return (
     <article key={id} className='person'>
-      <img src={image} alt={name}></img>
-      <div>
-        <h4>{name}</h4>
-        <p>{age} Years</p>
-      </div>
+
+        <img src={image} alt={name}></img>
+        <div>
+          <div>
+            <h4>{name}</h4>
+            <p>{age} Years</p>
+          </div>
+          <div>
+            <FaWindowClose style={{float:"right"}}/>
+          </div>
+        </div>
+      
+      
     </article>
   );
 };
