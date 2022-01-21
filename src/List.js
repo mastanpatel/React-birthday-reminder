b20658515ee61318b118ac04220def8509bf683f
Mastan Patel <m.mastanpatel@gmail.com>
2022-01-21 20:36:15 +0530

    added the List of Perople for bday app

--- src/List.js
+++ src/List.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const List = () => {
+const List = ({person}) => {
+
+  const  {id, name, age, image} = person;
+
   return (
-    <>
-      <h2>list component</h2>
-    </>
+    <article key={id} className='person'>
+      <img src={image} alt={name}></img>
+      <div>
+        <h4>{name}</h4>
+        <p>{age} Years</p>
+      </div>
+    </article>
   );
 };
 
